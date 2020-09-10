ce3eab0da8f1774b9b670586e579ffa2353b5e98
Benjamin Tan <demoneaux@gmail.com>
2020-09-10 22:44:03 +0800

    Install `lorri`.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -106,6 +106,9 @@
     # fluminurs
   ];
 
+  # Convenient shell integration with nix-shell.
+  services.lorri.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
