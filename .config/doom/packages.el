0049cf0a5ff95e9e6aa37db527458266cdc99f39
Benjamin Tan <demoneaux@gmail.com>
2020-09-13 13:58:33 +0800

    Install `org-gcal`.

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -5,6 +5,7 @@
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
 
+(package! org-gcal)
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
