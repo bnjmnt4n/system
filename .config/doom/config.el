d049a157a8989a272133bc33b7fc418ef42b833d
Benjamin Tan <demoneaux@gmail.com>
2020-09-19 11:32:40 +0800

    Enable `org-roam` and pretty org-mode.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -36,6 +36,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+(setq org-roam-directory ".")
 
 (after! org
     (setq org-capture-templates
