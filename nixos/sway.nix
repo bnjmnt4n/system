44105eaae267ea45f9dacf007a1c7df46d5a0536
Benjamin Tan <demoneaux@gmail.com>
2020-11-12 00:12:03 +0800

    Enable Wayland screensharing on Firefox.
    Seems like `MOZ_DBUS_REMOTE` is required.

--- nixos/sway.nix
+++ nixos/sway.nix
@@ -84,7 +84,11 @@ in
       # Fix for some Java AWT applications (e.g. Android Studio)
       export _JAVA_AWT_WM_NONREPARENTING=1
 
+      export XDG_SESSION_TYPE=wayland
       export XDG_CURRENT_DESKTOP=sway
+      export MOZ_ENABLE_WAYLAND=1
+      export MOZ_DBUS_REMOTE=1
+      export MOZ_USE_XINPUT2=1
     '';
     config = rec {
       inherit modifier;
--- nixos/wayland.nix
+++ nixos/wayland.nix
@@ -27,11 +27,4 @@
     sway-contrib.grimshot
     wf-recorder
   ];
-
-  home.sessionVariables = {
-    MOZ_ENABLE_WAYLAND = "1";
-    MOZ_USE_XINPUT2 = "1";
-    XDG_CURRENT_DESKTOP = "sway";
-    XDG_SESSION_TYPE = "wayland";
-  };
 }
