f5ff169dbda7ec6f51e98ac6e416350ecd98118a
Benjamin Tan <demoneaux@gmail.com>
2020-11-28 13:22:04 +0800

    Update Emacs.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs-wayland.url = "github:colemickens/nixpkgs-wayland";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
