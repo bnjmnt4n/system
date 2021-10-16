30e86fc41620566414f7448710927df5485aa1c3
Benjamin Tan <benjamin@dev.ofcr.se>
2021-10-16 20:18:51 +0800

    Neovim: fix lightspeed config

--- home/neovim/lua/bnjmnt4n/plugins/lightspeed.lua
+++ home/neovim/lua/bnjmnt4n/plugins/lightspeed.lua
@@ -1,10 +1,4 @@
-LightspeedRepeatFt = function(reverse)
-  local ls = require 'lightspeed'
-  ls.ft['instant-repeat?'] = true
-  ls.ft:to(reverse, ls.ft['prev-t-like?'])
-end
-
-vim.api.nvim_set_keymap('n', ';', '<cmd>lua LightspeedRepeatFt(false)<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('x', ';', '<cmd>lua LightspeedRepeatFt(false)<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', ',', '<cmd>lua LightspeedRepeatFt(true)<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('x', ',', '<cmd>lua LightspeedRepeatFt(true)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ';', '<Plug>Lightspeed_;_ft', { silent = true })
+vim.api.nvim_set_keymap('x', ';', '<Plug>Lightspeed_;_ft', { silent = true })
+vim.api.nvim_set_keymap('n', ',', '<Plug>Lightspeed_,_ft', { silent = true })
+vim.api.nvim_set_keymap('x', ',', '<Plug>Lightspeed_,_ft', {  silent = true })
