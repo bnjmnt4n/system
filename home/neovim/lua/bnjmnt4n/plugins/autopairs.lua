36eeb8c0c7963a3950d35cef499ef8138e316124
Benjamin Tan <benjamin@dev.ofcr.se>
2021-11-17 18:18:17 +0800

    Neovim: fix `nvim-autopairs`

--- home/neovim/lua/bnjmnt4n/plugins/autopairs.lua
+++ home/neovim/lua/bnjmnt4n/plugins/autopairs.lua
@@ -2,11 +2,3 @@
 require('nvim-autopairs').setup {
   disable_filetype = { 'TelescopePrompt', 'vim' },
 }
-
-require('nvim-autopairs.completion.cmp').setup {
-  map_cr = true,
-  -- Automatically select the first item
-  auto_select = true,
-  -- Insert `(` after completing a function or method item
-  map_complete = false,
-}
