a9e39dcaf147f4f0fff028a1c6896a368c46ac13
Benjamin Tan <benjamin@dev.ofcr.se>
2022-08-15 10:37:56 +0800

    Neovim: add more tree-sitter language grammars

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -28,6 +28,42 @@ let
     # TODO
     # nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSnapshotRollback default'
   '';
+  treeSitterLanguages = [
+    "bash"
+    "c"
+    "comment"
+    "cpp"
+    "css"
+    "fish"
+    "glimmer"
+    "go"
+    "gomod"
+    "graphql"
+    "haskell"
+    "html"
+    "java"
+    "javascript"
+    "jsdoc"
+    "json"
+    "ledger"
+    "lua"
+    "make"
+    "nix"
+    "ocaml"
+    "python"
+    "query"
+    "regex"
+    "ruby"
+    "rust"
+    "scss"
+    "svelte"
+    "toml"
+    "tsx"
+    "typescript"
+    "vue"
+    "yaml"
+    "zig"
+  ];
 in
 {
   home.activation.packerNvimSetup = lib.hm.dag.entryAfter [ "installPackages" "linkGeneration" ] ''
@@ -54,49 +90,26 @@ in
     '';
   };
 
-  xdg.configFile."nvim/lua" = {
-    source = ./lua;
-    onChange = ''
-      # TODO
-      # ${customNeovim}/bin/nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSnapshotRollback default'
-      ${customNeovim}/bin/nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-    '';
-  };
-
   xdg.dataFile."nvim/site/pack/packer/opt/telescope-fzf-native.nvim/build/libfzf.so".source = "${pkgs.telescope-fzf-native}/build/libfzf.so";
-  xdg.configFile."nvim/plugin/packer_snapshots/default".source = ./packer-snapshot.json;
 
-  xdg.configFile."nvim/parser/bash.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-bash}/parser";
-  xdg.configFile."nvim/parser/c.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-c}/parser";
-  xdg.configFile."nvim/parser/comment.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-comment}/parser";
-  xdg.configFile."nvim/parser/cpp.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-cpp}/parser";
-  xdg.configFile."nvim/parser/css.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-css}/parser";
-  xdg.configFile."nvim/parser/fish.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-fish}/parser";
-  xdg.configFile."nvim/parser/glimmer.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-glimmer}/parser";
-  xdg.configFile."nvim/parser/go.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-go}/parser";
-  xdg.configFile."nvim/parser/graphql.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-graphql}/parser";
-  xdg.configFile."nvim/parser/haskell.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-haskell}/parser";
-  xdg.configFile."nvim/parser/html.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-html}/parser";
-  xdg.configFile."nvim/parser/java.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-java}/parser";
-  xdg.configFile."nvim/parser/javascript.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-javascript}/parser";
-  xdg.configFile."nvim/parser/jsdoc.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-jsdoc}/parser";
-  xdg.configFile."nvim/parser/json.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-json}/parser";
-  xdg.configFile."nvim/parser/ledger.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-ledger}/parser";
-  xdg.configFile."nvim/parser/lua.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-lua}/parser";
-  xdg.configFile."nvim/parser/nix.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-nix}/parser";
-  xdg.configFile."nvim/parser/python.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-python}/parser";
-  xdg.configFile."nvim/parser/query.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-query}/parser";
-  xdg.configFile."nvim/parser/regex.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-regex}/parser";
-  xdg.configFile."nvim/parser/ruby.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-ruby}/parser";
-  xdg.configFile."nvim/parser/rust.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-rust}/parser";
-  xdg.configFile."nvim/parser/scss.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-scss}/parser";
-  xdg.configFile."nvim/parser/svelte.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-svelte}/parser";
-  xdg.configFile."nvim/parser/toml.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-toml}/parser";
-  xdg.configFile."nvim/parser/tsx.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-tsx}/parser";
-  xdg.configFile."nvim/parser/typescript.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-typescript}/parser";
-  xdg.configFile."nvim/parser/vue.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-vue}/parser";
-  xdg.configFile."nvim/parser/yaml.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-yaml}/parser";
-  xdg.configFile."nvim/parser/zig.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-zig}/parser";
+  xdg.configFile = {
+    "nvim/lua" = {
+      source = ./lua;
+      onChange = ''
+        # TODO
+        # ${customNeovim}/bin/nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSnapshotRollback default'
+        ${customNeovim}/bin/nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+      '';
+    };
+    "nvim/plugin/packer_snapshots/default".source = ./packer-snapshot.json;
+  } // (
+    builtins.listToAttrs (map (language: {
+      name = "nvim/parser/${language}.so";
+      value = {
+        source = "${pkgs.tree-sitter.builtGrammars."tree-sitter-${language}"}/parser";
+      };
+    }) treeSitterLanguages)
+  );
 
   # Shell aliases.
   programs.fish.shellAliases.v = "nvim";
