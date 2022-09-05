2738d8ca1a4bb2e8785ac7414390481af6dd55d9
Benjamin Tan <benjamin@dev.ofcr.se>
2022-09-05 11:15:42 +0800

    Templates: add Python flake template

---
+++ templates/python/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=12363fb6d89859a37cd7e27f85288599f13e49d9";
+    flake-utils.url = "github:numtide/flake-utils?rev=7e2a3b3dfd9af950a856d66b0a7d01e3c18aa249";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python37
+            nodePackages.pyright
+          ];
+        };
+      });
+}
