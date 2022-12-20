689b56f1eb7f58293cf37c7669777bd19ebb4fcc
Benjamin Tan <benjamin@dev.ofcr.se>
2022-12-20 14:30:08 +0800

    Nix: use settings attrset

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -2,21 +2,20 @@
 
 {
   nix = {
-    package = pkgs.nixUnstable;
     gc = {
       automatic = true;
       dates = "weekly";
       options = "--delete-older-than 30d";
     };
-    # Free up to 1GiB whenever there is less than 100MiB left.
-    extraOptions = ''
-      min-free = ${toString (100 * 1024 * 1024)}
-      max-free = ${toString (1024 * 1024 * 1024)}
+    settings = {
+      # Free up to 1GiB whenever there is less than 100MiB left.
+      min-free = 100 * 1024 * 1024;
+      max-free = 1024 * 1024 * 1024;
 
-      keep-outputs = true
-      keep-derivations = true
-      experimental-features = nix-command flakes
-    '';
+      keep-outputs = true;
+      keep-derivations = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
     trustedUsers = [ "root" "@wheel" ];
   };
 }
