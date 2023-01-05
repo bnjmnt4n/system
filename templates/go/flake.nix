b8a8cd1106edc347756f101400b78098ca29733c
Benjamin Tan <benjamin@dev.ofcr.se>
2023-01-05 16:33:32 +0800

    Update Nix flake templates

--- templates/go/flake.nix
+++ templates/go/flake.nix
@@ -1,18 +1,29 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=12363fb6d89859a37cd7e27f85288599f13e49d9";
-    flake-utils.url = "github:numtide/flake-utils?rev=7e2a3b3dfd9af950a856d66b0a7d01e3c18aa249";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=04f574a1c0fde90b51bf68198e2297ca4e7cccf4";
+    flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
-      in {
-       devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            go_1_17
-            gopls
+        package = pkgs.buildGoModule {
+          pname = "package";
+          version = "0.0.0";
+          src = pkgs.lib.cleanSource ./.;
+          vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+        };
+      in
+      {
+        packages.default = package;
+        apps.default = flake-utils.lib.mkApp {
+          drv = package;
+        };
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.gopls
           ];
         };
       });
--- templates/zig/flake.nix
+++ templates/zig/flake.nix
@@ -1,30 +1,19 @@
+# TODO: setup zls.
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?rev=12363fb6d89859a37cd7e27f85288599f13e49d9";
     flake-utils.url = "github:numtide/flake-utils?rev=7e2a3b3dfd9af950a856d66b0a7d01e3c18aa249";
-    zig-overlay.url = "github:arqv/zig-overlay";
-    zls = {
-      url = "github:bnjmnt4n/zls";
-      flake = false;
-    };
+    zig.url = "github:mitchellh/zig-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, zig-overlay, zls }:
+  outputs = { self, nixpkgs, flake-utils, zig }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
-        # Nix flakes are not fetched with submodules: 
-        # https://github.com/NixOS/nix/issues/4423
-        zlsWithSubmodules = builtins.fetchGit {
-          url = "https://github.com/bnjmnt4n/zls.git";
-          inherit (zls) rev;
-          submodules = true;
-        };
       in {
        devShell = pkgs.mkShell {
           buildInputs = [
-            zig-overlay.packages."${system}".master.latest
-            (import zlsWithSubmodules { inherit pkgs system; })
+            zig.packages."${system}"."0.10.0"
           ];
         };
       });
