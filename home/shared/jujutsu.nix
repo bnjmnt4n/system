0864876c2f36844088d7b54d58b4b5801185532b
Benjamin Tan <benjamin@dev.ofcr.se>
2024-12-25 16:16:19 +0800

    Jujutsu: Tweak configuration

--- home/shared/jujutsu.nix
+++ home/shared/jujutsu.nix
@@ -18,7 +18,7 @@
       diff.color-words.max-inline-alternation = 5;
       snapshot = {
         auto-update-stale = true;
-        auto-track = "glob:'*.*'";
+        auto-track = "glob:'**/*.*'";
       };
       templates = {
         log = "log_compact";
@@ -32,6 +32,31 @@
             "○"
           )
         '';
+        bookmark_list = ''
+          if(remote,
+            if(tracked,
+              "  " ++ separate(" ",
+                label("bookmark", "@" ++ remote),
+                format_tracked_remote_ref_distances(self),
+              ) ++ format_ref_targets(self),
+              label("bookmark", name ++ "@" ++ remote) ++ format_ref_targets(self),
+            ),
+            label("bookmark",
+              if(get_repository_github_url(),
+                hyperlink(concat(get_repository_github_url(), "/tree/", name), name),
+                name)) ++
+            if(present, format_ref_targets(self), " (deleted)"),
+          ) ++ "\n"
+        '';
+        tag_list = ''
+          label("tag",
+            if(get_repository_github_url(),
+              hyperlink(concat(get_repository_github_url(), "/releases/tag/", name), name),
+              name)) ++
+          format_ref_targets(self) ++ "\n"
+        '';
+        commit_summary = "format_commit_summary(self, bookmarks, tags)";
+        draft_commit_description = "draft_commit_description_verbose";
       };
       template-aliases = {
         # Used to link to repository branches and commits.
@@ -75,8 +100,8 @@
         "format_root_commit(root)" = ''
           separate(" ",
             label("immutable", format_short_change_id(root.change_id())),
-            label("root", "root()"),
             format_short_commit_id(root.commit_id()),
+            label("root", "root()"),
             root.bookmarks()
           ) ++ "\n"
         '';
@@ -90,6 +115,7 @@
                   if(commit.contained_in("trunk()"), "trunk"),
                   if(is_wip_commit_description(commit.description()), "wip"),
                   "empty",
+                  "description",
                 ),
                 "(empty)"
               )),
@@ -98,7 +124,7 @@
                 separate(" ",
                   if(commit.contained_in("trunk()"), "trunk"),
                   if(is_wip_commit_description(commit.description()), "wip")),
-                description.first_line()),
+                commit.description().first_line()),
               label(
                 separate(" ",
                   if(commit.contained_in("trunk()"), "trunk"),
@@ -116,6 +142,40 @@
             format_short_commit_id(commit.commit_id()),
           )
         '';
+        "format_ref_targets(ref)" = ''
+          if(ref.conflict(),
+            separate("\n",
+              " " ++ label("conflict", "(conflicted)") ++ ":",
+              ref.removed_targets().map(|c| "  - " ++ format_commit_summary(c)).join("\n"),
+              ref.added_targets().map(|c| "  + " ++ format_commit_summary(c)).join("\n"),
+            ),
+            ": " ++ format_commit_summary(ref.normal_target()),
+          )
+        '';
+        "format_commit_summary(commit)" = ''
+          separate(" ",
+            format_short_change_id_with_hidden_and_divergent_info(commit),
+            format_commit_id(commit),
+            separate(" ",
+              if(commit.conflict(), label("conflict", "(conflict)")),
+              format_commit_description(commit),
+            ),
+          )
+        '';
+        "format_commit_summary(commit, bookmarks, tags)" = ''
+          separate(" ",
+            format_short_change_id_with_hidden_and_divergent_info(commit),
+            format_commit_id(commit),
+            separate(commit_summary_separator,
+              format_commit_bookmarks(bookmarks),
+              format_commit_tags(tags),
+              separate(" ",
+                if(commit.conflict(), label("conflict", "(conflict)")),
+                format_commit_description(commit),
+              ),
+            ),
+          )
+        '';
         # TODO: This wrongly links to unpushed local bookmarks.
         "format_commit_bookmarks(bookmarks)" = ''
           if(get_repository_github_url(),
@@ -129,13 +189,35 @@
             bookmarks
           )
         '';
+        # TODO: This wrongly links to unpushed local tags.
+        "format_commit_tags(tags)" = ''
+          if(get_repository_github_url(),
+            tags.map(
+              |tag| if(
+                tag.remote() == "origin" || tag.remote() == "",
+                hyperlink(concat(get_repository_github_url(), "/releases/tag/", tag.name()), tag),
+                tag
+              )
+            ),
+            tags
+          )
+        '';
+        draft_commit_description = ''
+          concat(
+            description,
+            surround(
+              "\nJJ: This commit contains the following changes:\n", "",
+              indent("JJ:     ", diff.summary()),
+            ),
+          )
+        '';
         draft_commit_description_verbose = ''
           concat(
             description,
             separate("\n",
               "\nJJ: This commit contains the following changes:",
               indent("JJ:     ", diff.summary()),
-              "\nJJ: ignore-rest",
+              "JJ: ignore-rest",
               diff.git(),
             )
           )
@@ -146,13 +228,13 @@
             label(if(current_working_copy, "working_copy"),
               separate(" ",
                 format_short_change_id_with_hidden_and_divergent_info(self),
+                format_commit_id(self),
                 if(author.email(), author.email().local(), email_placeholder),
                 format_timestamp(committer.timestamp()),
                 format_commit_bookmarks(bookmarks),
-                tags,
+                format_commit_tags(tags),
                 working_copies,
                 if(git_head, label("git_head", "git_head()")),
-                format_commit_id(self),
                 if(conflict, label("conflict", "conflict")),
                 format_commit_description(self),
               ) ++ "\n",
@@ -166,13 +248,13 @@
               concat(
                 separate(" ",
                   format_short_change_id_with_hidden_and_divergent_info(self),
+                  format_commit_id(self),
                   format_short_signature(author),
                   format_timestamp(committer.timestamp()),
                   format_commit_bookmarks(bookmarks),
-                  tags,
+                  format_commit_tags(tags),
                   working_copies,
                   if(git_head, label("git_head", "git_head()")),
-                  format_commit_id(self),
                   if(conflict, label("conflict", "conflict")),
                 ) ++ "\n",
                 format_commit_description(self) ++ "\n",
@@ -187,13 +269,13 @@
               concat(
                 separate(" ",
                   format_short_change_id_with_hidden_and_divergent_info(self),
+                  format_commit_id(self),
                   format_short_signature(author),
                   format_timestamp(committer.timestamp()),
                   format_commit_bookmarks(bookmarks),
-                  tags,
+                  format_commit_tags(tags),
                   working_copies,
                   if(git_head, label("git_head", "git_head()")),
-                  format_commit_id(self),
                   if(conflict, label("conflict", "conflict")),
                 ) ++ "\n",
                 format_commit_description(self) ++ "\n",
@@ -236,8 +318,8 @@
         dg = [ "diff" "--tool" "idea" ];
         dd = [ "diff" "--git" "--config=ui.pager='delta'" ];
         ddl = [ "diff" "--git" "--config=ui.pager='delta --line-numbers'" ];
-        dv = [ "describe" "--config=templates.draft_commit_description=draft_commit_description_verbose" ];
-        descv = [ "describe" "--config=templates.draft_commit_description=draft_commit_description_verbose" ];
+        dt = [ "diff" "--tool" "difftastic" ];
+        descd = [ "describe" "--config=templates.draft_commit_description=draft_commit_description" ];
         diffg = [ "diff" "--tool" "idea" ];
         diffeditg = [ "diffedit" "--tool" "idea" ];
         l = [ "log" ];
@@ -250,6 +332,9 @@
         lmur = [ "log" "-r" "ancestors(unarchived(my_unmerged_remote()), 2) | trunk()" ];
         lnp = [ "log" "-r" "ancestors(unarchived(not_pushed()), 2) | trunk()" ];
         ls = [ "log" "-r" "ancestors(unarchived(stack(@)), 2) | trunk()" ];
+        lp = [ "log" "-T" "log_compact_no_summary" "--patch" ];
+        loneline = [ "log" "-T" "log_oneline" ];
+        lpatch = [ "log" "-T" "log_compact_no_summary" "--patch" ];
         lsummary = [ "log" "-T" "log_compact_no_summary" "--summary" ];
         n = [ "new" ];
         s = [ "show" ];
@@ -264,14 +349,13 @@
         abandon-merged = [ "abandon" "trunk()..@ & empty() ~ @ ~ merges() ~ visible_heads()" ];
         bump = [ "describe" "--reset-author" "--no-edit" ];
         bumpt = [ "describe" "--reset-author" "--no-edit" "tree(@)" ];
-        simplify = [ "simplify-parents" "-r" "reachable(@, ~ ::trunk())" ];
         sync = [ "rebase" "-d" "trunk()" "--skip-emptied" ];
-        synct = [ "rebase" "-s" "children(::trunk()) & mutable() ~ archived()" "-d" "trunk()" "--skip-emptied" ];
+        synct = [ "rebase" "-s" "children(::trunk()) & mine() & mutable() ~ archived()" "-d" "trunk()" "--skip-emptied" ];
         rebaset = [ "rebase" "-d" "trunk()" ];
         newt = [ "new" "trunk()" ];
       };
       merge-tools = {
-        difft = {
+        difftastic = {
           program = "${pkgs.difftastic}/bin/difft";
           diff-args = [ "--color=always" "$left" "$right" ];
           conflict-marker-style = "snapshot";
@@ -282,12 +366,18 @@
         };
         mergiraf = {
           program = "${pkgs.mergiraf}/bin/mergiraf";
-          merge-args = ["merge" "$base" "$left" "$right" "-o" "$output" "--fast"];
-          merge-conflict-exit-codes = [1];
+          merge-args = [ "merge" "$base" "$left" "$right" "-o" "$output" "--fast" ];
+          merge-conflict-exit-codes = [ 1 ];
           conflict-marker-style = "git";
         };
         idea = {
-          program = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin "${pkgs.jetbrains.idea-community}/Applications/IntelliJ IDEA CE.app/Contents/MacOS/idea";
+          program =
+            if pkgs.stdenv.hostPlatform.isDarwin
+            then
+              "${pkgs.writeShellScriptBin "idea-wrapper" ''
+                ${pkgs.jetbrains.idea-community}/Applications/IntelliJ\ IDEA\ CE.app/Contents/MacOS/idea $@ 2> /dev/null
+              ''}/bin/idea-wrapper"
+            else "${pkgs.jetbrains.idea-community}/bin/idea-community";
           diff-args = [ "diff" "$left" "$right" ];
           edit-args = [ "diff" "$left" "$right" ];
           merge-args = [ "merge" "$left" "$right" "$base" "$output" ];
@@ -342,13 +432,16 @@
         "hidden empty" = "black";
         "wip empty" = "yellow";
         "wip empty description placeholder" = "yellow";
-        "working_copy wip empty" = "green";
-        "working_copy description" = "green";
-        "working_copy description placeholder" = "green";
-        "working_copy wip empty description placeholder" = "green";
-        "working_copy wip description placeholder" = "green";
 
-        "log working_copy" = { bold = false; };
+        "working_copy" = { bold = false; };
+
+        "working_copy empty" = { fg = "green"; bold = true; };
+        "working_copy empty description placeholder" = "green";
+        "working_copy description" = { fg = "green"; bold = true; };
+        "working_copy description placeholder" = "green";
+        "working_copy wip description placeholder" = "green";
+        "working_copy wip empty description" = "green";
+        "working_copy wip empty description placeholder" = "green";
         "log working_copy bookmark" = "green";
         "log working_copy bookmarks" = "green";
         "log working_copy local_bookmarks" = "green";
