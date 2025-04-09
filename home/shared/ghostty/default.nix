1658540632f4f224dacd693898897a712cca7d76
Benjamin Tan <benjamin@dev.ofcr.se>
2025-04-09 19:21:23 +0800

    Ghostty: Shift configuration into Nix

--- home/shared/ghostty/default.nix
+++ home/shared/ghostty/default.nix
@@ -1,10 +1,25 @@
 {pkgs, ...}: {
-  home.packages =
-    if pkgs.stdenv.hostPlatform.isDarwin
-    # Installed via Homebrew for Darwin.
-    then []
-    else [pkgs.ghostty];
+  programs.ghostty = {
+    enable = true;
+    package =
+      if pkgs.stdenv.hostPlatform.isDarwin
+      # Installed via Homebrew for Darwin.
+      then null
+      else pkgs.ghostty;
+    settings = {
+      font-family = "Iosevka";
+      font-size = "20";
+      theme = "light:modus_operandi,dark:modus_vivendi";
+      cursor-style = "block";
+      window-padding-x = "10";
+      macos-option-as-alt = "true";
+      macos-titlebar-proxy-icon = "visible";
+      quick-terminal-animation-duration = "0";
+      keybind = "global:super+ctrl+shift+alt+space=toggle_quick_terminal";
+      shell-integration = "none"; # Handled by home-manager
+      shell-integration-features = "no-cursor";
+    };
+  };
 
   xdg.configFile."ghostty/themes".source = ./themes;
-  home.file."Library/Application Support/com.mitchellh.ghostty/config".source = ./config;
 }
